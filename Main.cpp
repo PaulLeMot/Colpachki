be975d930e65bda14fb0987e760b5462cb637862
Paul le Møt <paullemot@proton.me>
2026-01-28 23:34:57 +0400

    menu with text

--- Main.cpp
+++ Main.cpp
@@ -9,18 +9,32 @@
 #include <SDL3/SDL_main.h>
 #include <vector>
 class Colpachki {
-    SDL_Renderer* m_renderer;
-    TTF_Font* m_font;
     public:
-    Colpachki(SDL_Renderer* renderer, TTF_Font* font) : m_renderer(renderer), m_font(font){}
+    Colpachki(SDL_Renderer* renderer, TTF_Font* font) : m_renderer(renderer), m_font(font){
+    InitButtons();
+        if(m_font){
+            LoadButtonTextures();
+        }
+    }
+    ~Colpachki() {
+        FreeButtonTextures();
+    }
     void Render(const float width, const float height){
     SDL_SetRenderDrawColor(m_renderer, 0, 0, 100, 255);
     SDL_RenderClear(m_renderer);
     SDL_SetRenderDrawColor(m_renderer, 240, 240, 240, 21);
-    for(int i=0;i<3;++i){
-    SDL_FRect buttonRect = {width/3, height/8+(height/10+((height/8)*i)), width/3, height/10};
-    SDL_RenderFillRect(m_renderer, &buttonRect);
-    }
+        for(int i=0;i<3;++i){
+        SDL_FRect buttonRect = {width/3, height/8+(height/10+((height/8)*i)), width/3, height/10};
+        SDL_RenderFillRect(m_renderer, &buttonRect);
+            if (i < static_cast<int>(m_buttonTextures.size()) && m_buttonTextures[i]) {
+                float tw, th;
+                SDL_GetTextureSize(m_buttonTextures[i], &tw, &th);
+                float tx = buttonRect.x + (buttonRect.w - tw) / 2.0f;
+                float ty = buttonRect.y + (buttonRect.h - th) / 2.0f;
+                SDL_FRect textRect = {tx, ty, tw, th};
+                SDL_RenderTexture(m_renderer, m_buttonTextures[i], nullptr, &textRect);
+            }
+        }
     }
  
     /*void HandleEvent(const SDL_Event& event) {
@@ -35,14 +49,36 @@ class Colpachki {
         else if (event.type == SDL_EVENT_KEY_DOWN) {
             HandleKeyPress(event.key);
         }
-    }
+    }*/
     private:
     std::vector<std::string> m_buttons;
+    std::vector<SDL_Texture*> m_buttonTextures;
+    SDL_Renderer* m_renderer;
+    TTF_Font* m_font;
     void InitButtons() {
         m_buttons = {
             "Play", "Options", "Exit"
         };
     }
+    void LoadButtonTextures() {
+        m_buttonTextures.clear();
+        m_buttonTextures.resize(m_buttons.size(), nullptr);
+        SDL_Color color = {0, 0, 0, 255};
+        for (size_t i = 0; i < m_buttons.size(); ++i) {
+            SDL_Surface* surface = TTF_RenderText_Blended(m_font, m_buttons[i].c_str(), 0, color);
+            if (surface) {
+                m_buttonTextures[i] = SDL_CreateTextureFromSurface(m_renderer, surface);
+                SDL_DestroySurface(surface);
+            }
+        }
+    }
+    void FreeButtonTextures() {
+        for (SDL_Texture* tex : m_buttonTextures) {
+            if (tex) SDL_DestroyTexture(tex);
+        }
+        m_buttonTextures.clear();
+    }
+    /*
     int GetButtonAt(int mouseX, int mouseY) {
         int totalButtons = static_cast<int>(m_buttons.size());
         int columns = 6;
@@ -156,8 +192,6 @@ int main(int argc, char *argv[]){
     } else {
         std::cerr<<"problem with window size: "<<SDL_GetError()<<std::endl;
     }
-    std::cout<<width<<" "<<height<<std::endl;
-
     SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
     if(!renderer){
         std::cerr<<"renderer creation failed: "<<SDL_GetError()<<std::endl;
